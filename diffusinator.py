cf64000ec3ae05a06a0f4ff94339f19812035beb
Josh Arbon <jarbon@student.neumont.edu>
2025-03-12 14:46:59 -0600

    create array of numpy arrays

--- diffusinator.py
+++ diffusinator.py
@@ -1,5 +1,5 @@
 import torch
-# import numpy as np
+import numpy as np
 import model as m
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
@@ -7,12 +7,12 @@ print(f"Using {device} device")
 
 model = m.NeuralNetwork().to(device)
 criterion = torch.nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=.005)
+optimizer = torch.optim.SGD(model.parameters(), lr=.0005)
 
 input = torch.rand(1, 4 * 16 * 16)
 output = torch.rand(1, 4 * 16 * 16)
 print(output)
-
+images = []
 for epoch in range(20000):
     optimizer.zero_grad()
     outputs = model(input)
@@ -21,8 +21,10 @@ for epoch in range(20000):
     optimizer.step()
     
     if(epoch + 1) % 2000 == 0:
+        thingy = outputs.detach()
+        images.append(np.array(thingy))
         print(f'Epoch [{epoch+1}/{10000}], Loss: {loss.item():.4f}')
     if epoch == 19999:
         print(outputs)
-
+print(images)
 print("Training complete.")
--- model.py
+++ model.py
@@ -7,11 +7,11 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(4 * 16 * 16, 512),
+            nn.Linear(4 * 16 * 16, 2048),
             nn.ReLU(),
-            nn.Linear(512, 30),
+            nn.Linear(2048, 1024),
             nn.ReLU(),
-            nn.Linear(30, 4 * 16 * 16)
+            nn.Linear(1024, 4 * 16 * 16)
         )
     
     def forward(self, x):
